a949570b492aa14b63a6f93e189d88a993fddfb8
ShreyaGavali <121446993+ShreyaGavali@users.noreply.github.com>
2023-10-03 13:24:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function opeanNav(){
+    const nav = document.getElementById("menue-links").setAttribute(
+        "style","display: block; height: 2rem; width: 2rem;"
+    );
+}
+function closeNav(){
+    const nav = document.getElementById("menue-links").setAttribute(
+        "style","display:none;"
+    );
+}
